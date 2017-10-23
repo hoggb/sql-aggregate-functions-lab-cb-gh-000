6f324d732b5ad14171b1c18c4e4da86397794f59
Bret Hogg <hoggb@wwu.edu>
2017-10-23 18:03:06 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,9 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT * FROM students ORDER BY gpa LIMIT 1;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM STUDENTS;"
 end
 
 def total_tardies_for_all_students
